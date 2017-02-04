595a047194ae09156eaad5bcb49c0beb84dbf112
Steve Sun <sunweiguo02@163.com>
2017-02-04 09:35:18 +0800

    Add Test04

--- src/test04/Test.java
+++ src/test04/Test.java
@@ -1,4 +1,7 @@
 package test04;
+
+import java.util.Scanner;
+
 /*
   分析以下需求，并用代码实现：
 	(1)从键盘循环录入录入一个字符串,输入"end"表示结束
@@ -13,7 +16,51 @@ package test04;
 public class Test {
 
 	public static void main(String[] args) {
+		String input = inputString();
+		String delet = deleteString();
+		Object[] objectArray = deleteSubString(input,delet);
+		
+	}
 
+	//定义一个方法public Object[] deleteSubString(String str1,String str2)
+	private static Object[] deleteSubString(String input, String delet) {
+		Object[] object = new Object[2];
+		int beginIndex = 0;
+		int count =0;
+		String inputString = input;
+		while (inputString.contains("java")) {
+			beginIndex = inputString.indexOf("java");
+			inputString = inputString.substring(beginIndex + 4);
+			count++;
+		}
+		object[0] = input.replaceAll(delet, "");
+		object[1] = count;
+		return object;
+	}
+
+	//输入需要删除的字符串
+	private static String deleteString() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("请输入需要删除的字符串：");
+		String string = sc.nextLine(); 
+		return string;
+	}
+
+	//循环输入
+	private static  String inputString() {
+		StringBuffer string = new StringBuffer();
+		Scanner sc = new Scanner(System.in);
+        String str=null;  
+		while (true) {
+			System.out.println("请输入字符串 ："); 
+			String input = sc.nextLine(); 
+			if (input.equals("end")) {
+				System.out.println("输入结束");
+				break;
+			}
+			string.append(input);
+		}
+		return string.toString();
 	}
 
 }
